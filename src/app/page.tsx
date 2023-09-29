7fc09b0946586b57abb6797470a8538e8c5bcf70
mhmadamrii <interceptorghost4@gmail.com>
2023-09-29 18:36:57 +0700

    args

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Root() {
+  return (
+    <div className="bg-orange-950">
+      <h1>Hello world</h1>
+      <button className="bg-lime-400">simple button</button>
+    </div>
+  )
+}
