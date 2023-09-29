e7e631debf6eb24d403b1a78e4a1b4a19721de90
mhmadamrii <interceptorghost4@gmail.com>
2023-09-30 02:30:13 +0700

    working with tailwinds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,33 @@
-export default function Root() {
+type TData = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+const getData = async () => {
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/posts?_limit=10',
+  );
+  return res.json();
+};
+
+export default async function Root() {
+  const data = await getData();
+  console.log(data);
   return (
-    <div className="bg-orange-950">
-      <h1>Hello world</h1>
-      <button className="bg-lime-400">simple button</button>
-    </div>
+    <main>
+      {/* <div className="flex gap-8">
+        {data.map((item: TData, idx: number) => (
+          <div key={item.id} className="bg-slate-700 pr-2.5">
+            <h1>{item.body}</h1>
+          </div>
+        ))}
+      </div>
+ */}
+      <div className="text-2xl">
+        <h1>Hello world</h1>
+      </div>
+    </main>
   );
 }
