e65890f54a0356a2c8dbfd1de164d7a7b7c911c9
mhmadamrii <interceptorghost4@gmail.com>
2023-10-12 21:08:58 +0700

    soft

--- src/app/_components/Homepage.tsx
+++ src/app/_components/Homepage.tsx
@@ -1,61 +1,31 @@
 'use client';
 
-import React, { useState } from 'react';
-
-type TUserData = {
-  id: number;
-  name: string;
+const withRedColor = (WrappedComponent: any) => {
+  return (props: any) => (
+    <WrappedComponent {...props} isRed />
+  );
 };
 
-type THOmeChildProps = {
-  data: TUserData[];
-  onChange: (
-    e: React.ChangeEvent<HTMLInputElement>,
-  ) => void;
-};
-
-const HomeChild: React.FC<THOmeChildProps> = ({
-  data,
-  onChange,
-}): JSX.Element => {
-  console.log('data', data);
+const MyComponent = (props) => {
+  console.log('props', props);
   return (
-    <div>
-      <span>Homepage child</span>
-      <input
-        className="border-2 border-y-teal-200 px-3"
-        onChange={onChange}
-      />
+    <div
+      className={
+        props?.isRed ? 'bg-slate-500' : 'bg-zinc-700'
+      }
+    >
+      <h1 className="text-4xl">Hello world</h1>
     </div>
   );
 };
 
+const RedTextComponent = withRedColor(MyComponent);
+
 export default function Homepage() {
-  const [input, setInput] = useState<string>('');
-
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    console.log(e.target.value);
-    setInput(e.target.value);
-  };
-
-  const data = [
-    {
-      id: 1,
-      name: 'John',
-    },
-    {
-      id: 2,
-      name: 'Kevin',
-    },
-  ];
-
   return (
-    <div>
-      <HomeChild data={data} onChange={handleChange} />
-      <h1>Value: {input}</h1>
-      <input className="border-2" onChange={handleChange} />
+    <div className="container mx-auto mt-40">
+      <h1 className="text-4xl">HOC</h1>
+      <RedTextComponent />
     </div>
   );
 }
