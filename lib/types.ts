46847007d0cb2e84601f54cf592ee03ff9481951
mhmadamrii <interceptorghost4@gmail.com>
2023-10-14 12:40:13 +0700

    add nav links

---
+++ lib/types.ts
@@ -0,0 +1,17 @@
+export type TSidebarLinks = {
+    imgURL: string;
+    route: string;
+    label: string;
+}
+
+export type TProfileTabs = {
+    value: string;
+    label: string;
+    icon: string;
+}
+
+export type TCommunityTabs = {
+    value: string;
+    label: string;
+    icon: string;
+}
--- src/app/_components/shared/LeftSidebar.tsx
+++ src/app/_components/shared/LeftSidebar.tsx
@@ -1,7 +1,29 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { usePathname, useRouter } from 'next/navigation';
+import { sidebarLinks } from '../../../../constants';
+
 export default function LeftSidebar() {
+  const pathname = usePathname();
+  const router = useRouter();
   return (
-    <div>
-      <h1>Left sidebar</h1>
+    <div className="flex flex-col space-y-5">
+      {sidebarLinks.map((link, idx) => {
+        const isActive = pathname === link.route;
+
+        return (
+          <div
+            key={idx}
+            className={`${isActive ? 'bg-blue-500' : 'bg-black'} text-white`}
+          >
+            <Link href={link.route}>{link.label}</Link>
+            <Image src={link.imgURL} width={24} height={24} alt="link logo" />
+          </div>
+        );
+      })}
     </div>
   );
 }
