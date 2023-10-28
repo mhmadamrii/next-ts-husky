37f5712ffafe947d92f4c814c20c226f1bcc395f
mhmadamrii <interceptorghost4@gmail.com>
2023-10-28 20:31:18 +0700

    add tanstack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,7 @@
-import Card from '@/_components/Card';
-import MakeContact from '@/_components/MakeContact';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-const saveContact = async (contact: any) => {
-  'use server';
-  const response = await fetch('/api/contacts', {
-    method: 'POST',
-    // body: JSON.stringify(contact),
-    body: contact,
-  });
-
-  if (!response.ok) {
-    throw new Error(response.statusText);
-  }
-
-  return await response.json();
-};
-
-
-export default async function Root() {
-  const contacts = await prisma.contact.findMany();
+export default function Root() {
   return (
-    <div className="container mx-auto my-20">
-      <MakeContact saveContact={saveContact} />
-      {contacts.map((card, idx: number) => (
-        <Card
-          key={card.id}
-          firstName={card.firstName}
-          lastName={card.lastName}
-          email={card.email}
-          avatar={card.avatar}
-        />
-      ))}
+    <div>
+      <h1>Root</h1>
     </div>
   );
 }
