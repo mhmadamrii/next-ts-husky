62a8dffb8d47bbb4489ebb651a48bf4f7acf9129
mhmadamrii <interceptorghost4@gmail.com>
2023-10-29 13:52:15 +0700

    add generics

--- src/app/(root)/communities/page.tsx
+++ src/app/(root)/communities/page.tsx
@@ -1,7 +1,16 @@
-export default function Communities() {
+import * as React from 'react';
+
+interface IProps<T> {
+  title?: string;
+  jobs?: T[];
+}
+
+const Communities: React.FC = <T extends {}>({ title, jobs }: IProps<T>) => {
   return (
     <div>
-      <h1>welcome to the community sir</h1>
+      <h1>Hello world</h1>
     </div>
   );
-}
+};
+
+export default Communities;
