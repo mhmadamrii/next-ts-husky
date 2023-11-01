892e250b1200dad651816552887b1863e6fc724d
mhmadamrii <interceptorghost4@gmail.com>
2023-11-01 18:34:03 +0700

    add testing

--- __tests__/Home.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Root from '@/app/page'
-
-it('should render root page', () => {
-    render(<Root />)
-
-    const el = screen.getByText('Docs') // ACT
-    expect(el).tobe
-})
