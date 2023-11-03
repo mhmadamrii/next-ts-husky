2ea7e35e3964898d1210a5613c6dcc2dfbd41ccc
mhmadamrii <interceptorghost4@gmail.com>
2023-11-04 01:28:03 +0700

    change auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,13 @@
 // @ts-nocheck
 import { authMiddleware } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
-export default authMiddleware(() => {
-  return NextResponse.next();
-});
+// export default authMiddleware(() => {
+//   return NextResponse.next();
+// });
+export default authMiddleware({
+  publicRoutes: ["/api/webhook/clerk"],
+  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/api/webhook/clerk"]
+})
 
 // Stop Middleware running on static files
 export const config = {
